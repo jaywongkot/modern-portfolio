22fc02d138dc28a9309fd1e1f51211123947e996
Jay Wongkot <onzonstudios@gmail.com>
2020-09-28 11:40:37 -0300

    start a portfolio app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    # local
+    'pages',  # new
+    'portfolio',  # new
 ]
 
 MIDDLEWARE = [
