69b36beb7aa6286b231514da035b9810a10546ae
Jay Wongkot <onzonstudios@gmail.com>
2020-10-26 20:54:22 -0300

    format base.html with prettier

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,12 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .forms import CommentForm
 from .models import Post, Comment
 
-def blog_list(request):
-    posts = Post.objects.all().order_by('-created_on')
 
-    paginator = Paginator(posts, 4) #4 post in each page
-    page = request.GET.get('page')
+def blog_list(request):
+    posts = Post.objects.all().order_by("-created_on")
+
+    paginator = Paginator(posts, 4)  # 4 post in each page
+    page = request.GET.get("page")
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -18,20 +19,19 @@ def blog_list(request):
         posts = paginator.page(paginator.num_pages)
 
     context = {
-        'posts': posts,
-        'page': page,
+        "posts": posts,
+        "page": page,
     }
-    return render(request, 'blog.html', context)
+    return render(request, "blog.html", context)
+
 
 def blog_category(request, category):
-    posts = Post.objects.filter(
-        categories__name__contains=category
-    ).order_by(
-        'created_on'
+    posts = Post.objects.filter(categories__name__contains=category).order_by(
+        "created_on"
     )
 
-    paginator = Paginator(posts, 4) #4 post in each page
-    page = request.GET.get('page')
+    paginator = Paginator(posts, 4)  # 4 post in each page
+    page = request.GET.get("page")
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -41,19 +41,20 @@ def blog_category(request, category):
         posts = paginator.page(paginator.num_pages)
 
     context = {
-        'category': category,
-        'posts': posts,
-        'page': page,
+        "category": category,
+        "posts": posts,
+        "page": page,
     }
-    return render(request, 'blog_category.html', context)
+    return render(request, "blog_category.html", context)
+
 
 def blog_detail(request, pk):
-    template_name = 'blog_detail.html'
+    template_name = "blog_detail.html"
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.filter(active=True)
     new_comment = None
     # Comment posted
-    if request.method == 'POST':
+    if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
 
@@ -65,12 +66,12 @@ def blog_detail(request, pk):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    
+
     context = {
-        'post': post,
-        'comments': comments,
-        'new_comment': new_comment,
-        'comment_form': comment_form
+        "post": post,
+        "comments": comments,
+        "new_comment": new_comment,
+        "comment_form": comment_form,
     }
 
     return render(request, template_name, context)
--- static/js/main.js
+++ static/js/main.js
@@ -11,23 +11,23 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-	if (!showMenu) {
-		menuBtn.classList.add('close');
-		menu.classList.add('show');
-		menuNav.classList.add('show');
-		menuBranding.classList.add('show');
-		navItems.forEach((item) => item.classList.add('show'));
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach((item) => item.classList.add('show'));
 
-		// Set Menu State
-		showMenu = true;
-	} else {
-		menuBtn.classList.remove('close');
-		menu.classList.remove('show');
-		menuNav.classList.remove('show');
-		menuBranding.classList.remove('show');
-		navItems.forEach((item) => item.classList.remove('show'));
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach((item) => item.classList.remove('show'));
 
-		// Set Menu State
-		showMenu = false;
-	}
+    // Set Menu State
+    showMenu = false;
+  }
 }
