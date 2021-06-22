c8807256c2e8a052b8f57bad096727a4c721c944
Jay Wongkot <onzonstudios@gmail.com>
2021-06-22 15:00:33 -0300

    Add home_before_extend.html, about_before_extend.html and contact_extend_extend.html

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
+
 def index(request):
     """The home page for modern portfolio"""
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
     """The about page for modern portfolio"""
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def contact(request):
     """The contact page for modern portfolio"""
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
