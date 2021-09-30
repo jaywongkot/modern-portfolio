b426fa0215c0643c5b0def164ed850c2a8263f23
Jay Wongkot <onzonstudios@gmail.com>
2021-09-30 18:29:07 -0300

    updated virtual env to python 3.9.7 and updated all dependencies pacages

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,11 +3,12 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Project
 
-def project_list(request):
-    projects = Project.objects.all().order_by('-pk')
 
-    paginator = Paginator(projects, 4) # 4 projects in each page
-    page = request.GET.get('page')
+def project_list(request):
+    projects = Project.objects.all().order_by("-pk")
+
+    paginator = Paginator(projects, 4)  # 4 projects in each page
+    page = request.GET.get("page")
     try:
         projects = paginator.page(page)
     except PageNotAnInteger:
@@ -18,24 +19,28 @@ def project_list(request):
         projects = paginator.page(paginator.num_pages)
 
     context = {
-        'projects': projects,
-        'page': page,
+        "projects": projects,
+        "page": page,
     }
-    return render(request, 'portfolio.html', context)
+    return render(request, "portfolio.html", context)
+
 
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
     context = {
-        'project': project,
+        "project": project,
     }
-    return render(request, 'project_detail.html', context)
+    return render(request, "project_detail.html", context)
+
 
 def project_category(request, category):
-    projects = Project.objects.filter(categories__name__contains=category).order_by('-pk')
-    
-    paginator = Paginator(projects, 4) # 4 projects in each page
+    projects = Project.objects.filter(categories__name__contains=category).order_by(
+        "-pk"
+    )
+
+    paginator = Paginator(projects, 4)  # 4 projects in each page
     # Get to current page
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     try:
         projects = paginator.page(page)
     except PageNotAnInteger:
@@ -46,8 +51,8 @@ def project_category(request, category):
         projects = paginator.page(paginator.num_pages)
 
     context = {
-        'projects': projects,
-        'category': category,
-        'page': page,
+        "projects": projects,
+        "category": category,
+        "page": page,
     }
-    return render(request, 'project_category.html', context)
+    return render(request, "project_category.html", context)
